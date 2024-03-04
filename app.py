8e01b16f05e41723666eb0e977c0ad65b4b61340
shailesh2210 <shaileshgaddam22@gmail.com>
2024-03-04 16:19:08 +0530

    Wheat price Prediction

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+# model = pickle.load(open("rf_model.pkl", "rb"))
+model = pickle.load(open("lg_model.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+    state = int(request.form["state"])
+    district = int(request.form["district"])
+    variety = int(request.form["variety"])
+    min_price = int(request.form["min_price"])
+    max_price = int(request.form["max_price"])
+
+    data = ([[state, district, variety, min_price, max_price]])
+    prediction = model.predict(data)  # Extracting the prediction from the array
+    print(prediction)
+
+    return render_template("predict.html", prediction = np.round(prediction,2))
+
+    # return render_template("predict.html", prediction="No data submitted.")
+
+if __name__ == "__main__":
+    app.run(debug=True)
